3bc310da0655ed371323a67163e9de62f8db67c8
Mathew Gettins <S00240421@atu.ie>
2023-02-11 15:06:49 +0000

    Add project files.

---
+++ Part 1/Program.cs
@@ -0,0 +1,39 @@
+﻿using System.Data;
+
+namespace Part_1
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine(CalculateTip(26.95, "good"));      // Call the `CalculateTip` method and print the result to the console
+        }
+
+        public static int CalculateTip(double amount, string rating)
+        {
+            rating = rating.ToLower();                           // Convert the rating to lower case for case-insensitivity
+                                                          
+            switch (rating)                                      // Use a switch statement to get tip based on rating
+            {
+                case "excellent":                                // Check if the rating is "excellent" and return the tip amount
+                    return (int)Math.Ceiling(amount * 0.2);      
+
+                case "great":
+                    return (int)Math.Ceiling(amount * 0.15);
+
+                case "good":
+                    return (int)Math.Ceiling(amount * 0.1);
+
+                case "poor":
+                    return (int)Math.Ceiling(amount * 0.05);
+
+                case "terrible":
+                                                                 // No tip for terrible service
+                    return 0;
+                default:
+                                                                 // Return -1 for unrecognized service ratings
+                    return -1;
+            }
+        }
+    }
+}
---
+++ Part 2/Program.cs
@@ -0,0 +1,51 @@
+﻿using System.Net.NetworkInformation;
+
+namespace Part_2
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            string status = "";
+            int years = 0; 
+            double hours = 0;
+            status = "";
+
+            string[] name = new string[1];
+            double[] wages= new double[1];
+
+
+            for (int i = 0; i < name.Length; i++)
+            {
+                Console.Write("Enter Name         : ");
+                name[i] = Console.ReadLine();
+
+                Console.Write("Status (W/H)       : ");
+                status = Console.ReadLine();    
+
+                Console.Write("Enter hours        : ");
+                hours = double.Parse(Console.ReadLine());
+
+                Console.Write("Enter years    : ");
+                years = int.Parse(Console.ReadLine());
+
+                Console.Write($"{name[i]} you have worked {hours} and your wages are {Wages}"); 
+
+            }
+
+        static int Wages()
+            {   
+                if (status = "w")
+                {
+
+                }
+                return 0;
+
+            }
+
+
+
+
+        }
+    }
+}
